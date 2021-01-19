0cca1cbab36771a57400cb1e5f2eb4da83dbc01e
HeeRyun, Jin <jins4218@gmail.com>
2021-01-20 01:50:46 +0900

    갤러리 리스트 및 특정 갤러리에 작성된 글 / 댓글의 총 갯수를 구하는 Function 구현

--- DCCleaner_GUI.py
+++ DCCleaner_GUI.py
@@ -1,14 +1,15 @@
 import lxml.html
+import re
 import requests
 import sys
 
+from bs4 import BeautifulSoup
 from configparser import ConfigParser
 
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
 from qt_material import apply_stylesheet
-
 
 form_class = uic.loadUiType("DCCleaner.ui")[0]
 
@@ -32,6 +33,9 @@ def initSession():
 class MyWindow(QMainWindow, form_class):
     isSave = False
 
+    commentGallNo = []
+    postGallNo = []
+
     config = ConfigParser()
 
     def __init__(self):
@@ -40,16 +44,22 @@ class MyWindow(QMainWindow, form_class):
         initSession()
         self.loadConf()
         self.isSaveAccount.stateChanged.connect(self.mgrAccount)
+        self.commentGallList.setDisabled(True)
+        self.delComment.setDisabled(True)
+        self.postGallList.setDisabled(True)
+        self.delPost.setDisabled(True)
         self.loginButton.clicked.connect(self.dcLogin)
         self.devInfoButton.clicked.connect(self.devInfoMsg)
         self.idBox.returnPressed.connect(self.focusEvent)
         self.pwBox.returnPressed.connect(self.loginButton.click)
         self.idBox.setFocus()
+        self.commentGallList.currentIndexChanged.connect(self.commentGallSelectionChanged)
+        self.postGallList.currentIndexChanged.connect(self.postGallSelectionChanged)
 
     def closeEvent(self, event):
         result = QMessageBox.question(self, 'Are you sure to Exit?',
-                                           "디시 클리너를 종료하시겠습니까?", QMessageBox.Yes |
-                                           QMessageBox.No, QMessageBox.No)
+                                      "디시 클리너를 종료하시겠습니까?", QMessageBox.Yes |
+                                      QMessageBox.No, QMessageBox.No)
         if result == QMessageBox.Yes:
             event.accept()
         else:
@@ -119,6 +129,7 @@ class MyWindow(QMainWindow, form_class):
                 self.config.set('Account', 'id', dcid)
                 self.config.set('Account', 'pw', dcpw)
                 self.writeConf()
+            self.getGallList(dcid)
 
     def dcLogout(self):
         initSession()
@@ -126,7 +137,83 @@ class MyWindow(QMainWindow, form_class):
         self.loginButton.setText("로그인")
         self.idBox.setDisabled(False)
         self.pwBox.setDisabled(False)
+        self.commentGallList.clear()
+        self.commentGallList.setDisabled(True)
+        self.delComment.setDisabled(True)
+        self.postGallList.clear()
+        self.postGallList.setDisabled(True)
+        self.delPost.setDisabled(True)
         self.loginButton.clicked.connect(self.dcLogin)
+
+    def getGallList(self, dcid):
+        # Get Gallog
+        comment_gallog = sess.get("https://gallog.dcinside.com/" + dcid + "/comment")
+        comment_gallog_parsed = BeautifulSoup(comment_gallog.text, "lxml")
+
+        post_gallog = sess.get("https://gallog.dcinside.com/" + dcid + "/posting")
+        post_gallog_parsed = BeautifulSoup(post_gallog.text, "lxml")
+
+        # Parse optionbox for Listing All Galleries
+        comment_option_box = comment_gallog_parsed.find_all(class_="option_box")[1]
+        comment_option_box = comment_option_box.find_all("li")
+
+        post_option_box = post_gallog_parsed.find_all(class_="option_box")[1]
+        post_option_box = post_option_box.find_all("li")
+
+        comment_gall_list = ["전체 갤러리"]
+        self.commentGallNo += [dcid + "/comment"]
+        for gall in range(1, len(comment_option_box)):
+            comment_gall_list += [comment_option_box[gall].text]
+            self.commentGallNo += [comment_option_box[gall]['onclick'][15:-1]]
+
+        post_gall_list = ["전체 갤러리"]
+        self.postGallNo += [dcid + "/posting"]
+        for gall in range(1, len(post_option_box)):
+            post_gall_list += [post_option_box[gall].text]
+            self.postGallNo += [post_option_box[gall]['onclick'][15:-1]]
+
+        for s in comment_gall_list:
+            self.commentGallList.addItem(s)
+        self.commentGallList.setDisabled(False)
+        self.delComment.setDisabled(False)
+
+        for s in post_gall_list:
+            self.postGallList.addItem(s)
+        self.postGallList.setDisabled(False)
+        self.delPost.setDisabled(False)
+
+    def commentGallSelectionChanged(self):
+        idx = self.commentGallList.currentIndex()
+
+        # Get Gallog
+        gallog = sess.get("https://gallog.dcinside.com/" + self.commentGallNo[idx])
+        gallog_parsed = lxml.html.fromstring(gallog.text)
+
+        # Get num
+        if idx == 0:
+            num = gallog_parsed.xpath('//*[@id="container"]/article/div/section/div[1]/header/div/div[1]/button[1]/span')[0].text.replace(",", "")
+        else:
+            num = gallog_parsed.xpath('//*[@id="container"]/article/div/section/div[1]/header/div/h2/span[3]')[0].text.replace(",", "")
+        num = re.findall("\d+", num)[0]
+
+        self.totalComment.setText("전체 댓글 : %s개" % num)
+
+    def postGallSelectionChanged(self):
+        idx = self.postGallList.currentIndex()
+
+        # Get Gallog
+        gallog = sess.get("https://gallog.dcinside.com/" + self.postGallNo[idx])
+        gallog_parsed = lxml.html.fromstring(gallog.text)
+
+        # Get num
+        if idx == 0:
+            num = gallog_parsed.xpath('//*[@id="container"]/article/div/section/div[1]/header/div/div[1]/button[1]/span')[0].text.replace(",", "")
+        else:
+            num = gallog_parsed.xpath('//*[@id="container"]/article/div/section/div[1]/header/div/h2/span[3]')[0].text.replace(",", "")
+        num = re.findall("\d+", num)[0]
+
+        self.totalPost.setText("전체 게시글 : %s개" % num)
+
 
     def mgrAccount(self):
         if self.isSaveAccount.isChecked():
